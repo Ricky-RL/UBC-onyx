b2b63d8712c285043a89cd9138793b7466804092
Ricky-RL <rickylin543@gmail.com>
2022-11-13 00:47:59 -0800

    added nav bar
    added buttons to the nav bar

--- ubc-onyx/src/components/navbar/navbar.jsx
+++ ubc-onyx/src/components/navbar/navbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
-import logo from '../../assets/ubcea_log.png';
+import logo from '../../assets/ubcea_logo.png';
 import './navbar.css';
+// import {BrowserRouter as Router, Link} from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-links">
         <div className="navbar-links_logo">
-          <img className="logo" src={logo} alt="logo" />
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="navbar-links_container">
+          <p><a href="#About">About</a></p>
+          <p><a href="#UBCEA">UBC Esports Association</a></p>
+          <p><a href="#Contact">Contact Us</a></p>
         </div>
       </div>
     </div>
