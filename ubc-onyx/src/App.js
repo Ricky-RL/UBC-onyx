e41bccbb7208a9084f9071a1c0694c04f1c2700c
Ricky-RL <rickylin543@gmail.com>
2022-11-17 22:35:59 -0800

    not working

--- ubc-onyx/src/App.js
+++ ubc-onyx/src/App.js
@@ -1,72 +1,49 @@
 import React from 'react'
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
-import { CTA, Brand, Navbar, Profile } from './components';
+import { CTA, Brand, Navbar, Profile, Home, About, Contact } from './components';
 import './App.css';
+// import { Routes, Route, useNavigate, Router, BrowserRouter, Link, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { BrowserRouter as Router, Switch, 
   Route, Redirect,} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
+
   return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-      </div>
-      <div className="profiles">
-        <div className="child">
-          <Profile lane="TOP"
-            char="profile_kayle"
-            ign="WINNABLEE" />
-        </div>
-
-        <div className="child">
-          <Profile lane="JUNGLE"
-            char="profile_graves"
-            ign="Chiv" />
-        </div>
-
-        <div className="child">
-          <Profile lane="MID"
-            char="profile_zoe"
-            ign="Lexi8878" />
-        </div>
-
-        <div className="child">
-          <Profile lane="ADC"
-            char="profile_jinx"
-            ign="R1 Chen" />
-        </div>
-
-        <div className="child">
-          <Profile lane="Support"
-            char="profile_nautilus"
-            ign="GorillA" />
-        </div>
-
-        <div className="child">
-          <Profile lane="Top"
-          char="profile_gragas"
-          ign="DOCTOR NEFARIO"/>
-        </div>
-
-        <div className="child">
-          <Profile lane="ADC"
-          char="profile_lucian"
-          ign="Shellybeans"/>
-        </div>
-
-        <div className="child">
-        <Profile lane="Coach"
-        char="profile_trundle"
-        ign="Julian"/>
-        </div>
-      </div>
-
-
-
-
-    </div>
-  )
+    <>
+    {/* This is the alias of BrowserRouter i.e. Router */}
+    <Router>
+      <Switch>
+        {/* This route is for home component 
+        with exact path "/", in component props 
+        we passes the imported component*/}
+        <Route exact path="/" component={Home} />
+          
+        {/* This route is for about component 
+        with exact path "/about", in component 
+        props we passes the imported component*/}
+        <Route path="/about" component={About} />
+          
+          
+        {/* If any route mismatches the upper 
+        route endpoints then, redirect triggers 
+        and redirects app to home component with to="/" */}
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  </>
+    // <div className="App">
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       {/* <Route path="/sfu_twitter_feed" element={<SFUTwitterFeed />} />
+    //       <Route path="/road_conditions" element={<RoadCondition />} /> */}
+    //     </Routes>
+    //   </BrowserRouter>
+    // </div>
+  );
 }
 
 
---
+++ ubc-onyx/src/components/home/Home.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+// import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './../containers';
+import { CTA, Brand, Navbar, Profile, About, Contact } from './../../components';
+
+// import Profile from "./../profile/Profile";
+// import Profile from "./../profile/Profile";
+
+
+import { Routes, Route, useNavigate, Router, BrowserRouter, Link, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './home.css';
+function Home () {
+  return (
+    <div className="App">
+      <p>hasd</p>
+      {/* <div className="gradient__bg">
+        <Navbar />
+      </div>
+      <div className="profiles">
+        <div className="child">
+          <Profile lane="TOP"
+            char="profile_kayle"
+            ign="WINNABLEE" />
+        </div>
+
+        <div className="child">
+          <Profile lane="JUNGLE"
+            char="profile_graves"
+            ign="Chiv" />
+        </div>
+
+        <div className="child">
+          <Profile lane="MID"
+            char="profile_zoe"
+            ign="Lexi8878" />
+        </div>
+
+        <div className="child">
+          <Profile lane="ADC"
+            char="profile_jinx"
+            ign="R1 Chen" />
+        </div>
+
+        <div className="child">
+          <Profile lane="Support"
+            char="profile_nautilus"
+            ign="GorillA" />
+        </div>
+
+        <div className="child">
+          <Profile lane="Top"
+          char="profile_gragas"
+          ign="DOCTOR NEFARIO"/>
+        </div>
+
+        <div className="child">
+          <Profile lane="ADC"
+          char="profile_lucian"
+          ign="Shellybeans"/>
+        </div>
+
+        <div className="child">
+        <Profile lane="Coach"
+        char="profile_trundle"
+        ign="Julian"/>
+        </div>
+      </div> */}
+
+
+
+
+    </div>
+  )
+}
+
+export default Home;
